b737e11b2a44cc432fc816e12afe962c78b521b1
lakshaybamel <lakbamel@gmail.com>
2025-09-04 00:48:51 +0530

    created array in java and .gitignore file to ignore .class files

---
+++ Arrays/ArraysCC.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ArraysCC {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int numbers[] = {1, 2, 3};
+
+        System.out.println("Program ran successfully!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DSA-JAVA
 Data Structures and Algorithms in Java – concept codes from Apna College (Delta Batch)
+<br>
+Author - Lakshay Bamel
