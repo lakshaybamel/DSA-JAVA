f1e0ef37b006129afbc205c461f3b784000bdac5
lakshaybamel <lakbamel@gmail.com>
2025-09-04 03:13:51 +0530

    Done the operation Input, Output, & update

--- Arrays/ArraysCC.java
+++ Arrays/ArraysCC.java
@@ -2,10 +2,25 @@ import java.util.*;
 
 public class ArraysCC {
     public static void main(String[] args) {
-        int marks[] = new int[50];
-        int numbers[] = {1, 2, 3};
-        String names[] = {"Lakshay", "Harsh"};
 
-        System.out.println("Program ran successfully!");
+        int marks[] = new int[50];
+
+        System.out.println("Length of array = " + marks.length);
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("Phy : " + marks[0]);
+        System.out.println("Chem : " + marks[1]);
+        System.out.println("Math : " + marks[2]);
+
+        marks[2] += 10;
+        System.out.println("Math updated : " + marks[2]);
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("percentage = " + percentage + "%");
     }
 }
