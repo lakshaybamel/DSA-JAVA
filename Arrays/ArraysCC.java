182597345af8e7618922282232329d84ed9bd840
lakshaybamel <lakbamel@gmail.com>
2025-09-04 01:08:50 +0530

    Created array of String datatype

--- Arrays/ArraysCC.java
+++ Arrays/ArraysCC.java
@@ -4,6 +4,7 @@ public class ArraysCC {
     public static void main(String[] args) {
         int marks[] = new int[50];
         int numbers[] = {1, 2, 3};
+        String names[] = {"Lakshay", "Harsh"};
 
         System.out.println("Program ran successfully!");
     }
