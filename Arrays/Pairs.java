0c1e9d857c88785b894a50e586b3120d19567138
lakshaybamel <lakbamel@gmail.com>
2025-09-05 15:27:46 +0530

    Arrays: update pairs program to show total count

--- Arrays/Pairs.java
+++ Arrays/Pairs.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Pairs {
 
     public static void printPairs(int arr[]) {
+        int count = 0;
         for(int i = 0; i < arr.length; i++) {
             int current_pos = arr[i]; //2, 4, 6, 8, 10
             for(int j = i + 1; j < arr.length; j++){
                 System.out.print("(" + current_pos + "," + arr[j] + ")");
+                count++;
             }
             System.out.println();
         }
+        System.out.println("Total pairs = " + count);
     }
 
     public static void main(String []args) {
