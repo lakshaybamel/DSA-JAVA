eb869c5834660b4111154ccfb313fd90460cfd07
lakshaybamel <lakbamel@gmail.com>
2025-09-05 15:22:24 +0530

    Arrays: add program to print all pairs

---
+++ Arrays/Pairs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pairs {
+
+    public static void printPairs(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            int current_pos = arr[i]; //2, 4, 6, 8, 10
+            for(int j = i + 1; j < arr.length; j++){
+                System.out.print("(" + current_pos + "," + arr[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String []args) {
+        int arr[] = {2, 4, 6, 8, 10};
+        printPairs(arr);
+    }
+}
