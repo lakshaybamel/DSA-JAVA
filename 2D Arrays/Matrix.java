d839c8232cab81983e01d269d37f6db36c4145c8
lakshaybamel <lakbamel@gmail.com>
2025-09-10 18:59:17 +0530

    2D Arrays: Implement linear search in Matrix.java

--- 2D Arrays/Matrix.java
+++ 2D Arrays/Matrix.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
 public class Matrix {
+    public static int[] search (int arr[][], int target){
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == target){
+                    System.out.print("Element fount at index["+i+"]["+j+"]");
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+
     public static void main (String []args){
         Scanner sc = new Scanner(System.in);
 
         int arr[][] = new int[3][3];
+        int target = 5;
 
         for (int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[0].length; j++) {
@@ -20,5 +33,7 @@ public class Matrix {
             }
             System.out.println();
         }
+
+        search(arr, target);
     }
 }
