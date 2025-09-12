27ae7c15ca60832b0e88cb61d4af4166f3ee66fb
lakshaybamel <lakbamel@gmail.com>
2025-09-12 20:25:14 +0530

    Strings: Add StringBasics.java with declaration, methods, and operations

---
+++ Strings/StringBasics.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class StringBasics {
+    public static void main(String[] args) {
+
+        //Decleration
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("abcd");
+
+        //Strings are IMMUTABLE
+
+        //user input
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        //String length
+        String city = "Gurugram";
+        System.out.println(city.length());
+
+        //concatenation
+        String firstName = "Lakshay";
+        String lastName = "Bamel";
+        String fullName =  firstName + " " + lastName;
+        System.out.println(fullName);
+
+        //String charAt method
+        String name = "Lakshay";
+        System.out.println(name.charAt(0));
+
+        //Access all String elements
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+}
