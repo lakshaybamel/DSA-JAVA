b18710b3e72cc7f006b192441d6999564f2b50f4
lakshaybamel <lakbamel@gmail.com>
2025-09-20 17:20:38 +0530

    Recursion: Add Fibonacci sequence program

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fn = fib(n-1) + fib(n-2);
+        return fn;
+    }
+    public static void main(String[] main) {
+        int n = 10;
+        System.out.println(fib(n));
+    }
+}
