5da8c0a79e1582e70740c56d24f644d79c0e21ac
lakshaybamel <lakbamel@gmail.com>
2025-09-20 15:17:57 +0530

    Recursion: Add printDec() to print numbers in decreasing order

---
+++ Recursion/recursionBasics.java
@@ -0,0 +1,16 @@
+public class recursionBasics {
+    //print numbers in dec. order using recursion 
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+    
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
