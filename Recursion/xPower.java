55657cd4f7ab27b2395ac0e282d2ac3120c99772
lakshaybamel <lakbamel@gmail.com>
2025-09-20 21:14:59 +0530

    Recursion: Update power function with optimized O(log n) version

--- Recursion/xPower.java
+++ Recursion/xPower.java
@@ -1,12 +1,26 @@
 public class xPower{
+    //time - O(n)
     public static int pow(int x, int n) {
         if(n == 0) return 1;
         int xn = x * pow(x, n-1);
         return xn;
     }
 
+    // Optimized recursion O(log n)
+    public static int opPow(int x, int n){
+        if(n == 0) return 1;
+
+        int halfPower = opPow(x, n/2);         // compute x^(n/2)
+        int halfPowerSquare = halfPower * halfPower;
+
+        if(n % 2 != 0) {                       // if n is odd
+            return x * halfPowerSquare;
+        }
+        return halfPowerSquare;                // if n is even
+    }
+
     public static void main(String[] args) {
         int x = 2, n = 10;
-        System.out.println(pow(x, n));
+        System.out.println(opPow(x, n));
     }
 }
